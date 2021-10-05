9a93c74d2d52299382fbc0f91b7b421df78d4d60
Jorge Pereda <jorge.pereda@chasacademy.se>
2021-10-05 23:59:24 +0200

    generating all the components

--- HTML5/src/app/app.module.ts
+++ HTML5/src/app/app.module.ts
@@ -5,11 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ImagesComponent } from './images/images.component';
+import { HeaderComponent } from './header/header.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TopNewsComponent } from './top-news/top-news.component';
+import { LastNewsComponent } from './last-news/last-news.component';
+import { TopBlockComponent } from './top-block/top-block.component';
+import { AboutBlockComponent } from './about-block/about-block.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImagesComponent
+    ImagesComponent,
+    HeaderComponent,
+    GalleryComponent,
+    TopNewsComponent,
+    LastNewsComponent,
+    TopBlockComponent,
+    AboutBlockComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
