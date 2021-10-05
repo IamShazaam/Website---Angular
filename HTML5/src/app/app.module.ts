dde54a5337cce3dc1037409a4fa0ae88500f66fe
Jorge Pereda <jorge.pereda@chasacademy.se>
2021-10-05 23:20:32 +0200

    installation finished

---
+++ HTML5/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ImagesComponent } from './images/images.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
